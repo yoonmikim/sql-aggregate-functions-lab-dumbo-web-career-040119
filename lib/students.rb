1a6812979791411ce94ba9f4e41093d6a730aa7f
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-07 22:43:31 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,21 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
+  .max
 end
 
 def lowest_student_gpa
+  .min
 end
 
 def average_student_gpa
+  sum / student.length
 end
 
 def total_tardies_for_all_students
+  sum
 end
 
 def average_gpa_for_9th_grade
+  sum 
 end
